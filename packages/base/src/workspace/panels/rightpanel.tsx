baf411ddf6a656f0baa8de3d02821eb5808778c1
Benjamin Szeghy <benjamin.szeghy@berkeley.edu>
2026-06-04 20:36:43 -0700

    add clip function to processing generators

--- packages/base/src/workspace/panels/rightpanel.tsx
+++ packages/base/src/workspace/panels/rightpanel.tsx
@@ -229,7 +229,8 @@ export const RightPanel: React.FC<IRightPanelProps> = props => {
                       id: lid,
                       name: layer.name,
                       source: props.model.getSource(layer.parameters?.source)?.parameters?.path,
-                      type: layer.type
+                      type: layer.type,
+                      vectorType: layer.type === 'VectorLayer' ? layer.parameters?.symbologyState?.geometryType : undefined
                     }))}
                     jgisPath={props.model.filePath.split('/').pop() ?? ''}
                     onExecute={(output: string) => {
